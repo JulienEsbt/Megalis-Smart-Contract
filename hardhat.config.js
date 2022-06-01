6e0f9548bc6867170eefb89091c0fbe3c0fbfd0b
esterbetjstage <julien.esterbet@sib.fr>
2022-06-01 17:17:58 +0200

    Ajout de la fonction publish du smart contract, création du fichier test (à finir quand le smart contract le sera), création du fichier deploy.js.

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,16 +4,22 @@ require("dotenv").config();
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-const { ALCHEMY_API_URL, PRIVATE_KEY } = process.env
+const { PRIVATE_KEY, ROPSTEN_ALCHEMY_API_KEY, RINKEBY_ALCHEMY_API_KEY, INFURA_API_KEY } = process.env
 
 module.exports = {
   solidity: "0.8.4",
-  defaultNetwork: "rinkeby",
+  defaultNetwork: "ropsten",
   networks: {
     hardhat: {},
+    ropsten: {
+      //url: `https://eth-ropsten.alchemyapi.io/v2/${ROPSTEN_ALCHEMY_API_KEY}`,
+      url: `https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [`${PRIVATE_KEY}`]
+    },
     rinkeby: {
-      url: ALCHEMY_API_URL,
-      accounts: [`0x${PRIVATE_KEY}`],
+      //url: `https://eth-rinkeby.alchemyapi.io/v2/${RINKEBY_ALCHEMY_API_KEY}`,
+      url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [`${PRIVATE_KEY}`],
     }
   }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,11 @@
+// Ropsten
 async function main() {
     const [deployer] = await ethers.getSigners();
     console.log("Deploying contracts with the account:", deployer.address);
 
-    const Megalis = await ethers.getContractFactory("megalisV1");
-
-    const megalisContract = await Megalis.deploy();
+    const megalisFactory = await ethers.getContractFactory("megalisV1");
+    const megalisContract = await megalisFactory.deploy();
+    //await megalisContract.deployed();
     console.log("Contract address:", megalisContract.address);
 }
 
@@ -14,3 +15,26 @@ main()
         console.error(error);
         process.exit(1);
     });
+
+//Rinkeby
+/*const hre = require("hardhat");
+const main = async () => {
+    const [deployer] = await hre.ethers.getSigners();
+    console.log("Deploying contracts with the account:", deployer.address);
+    const megalisFactory = await hre.ethers.getContractFactory("megalisV1");
+    const megalisContract = await megalisFactory.deploy();
+    //await megalisContract.deployed();
+    console.log("Megalis Smart Contract address: ", megalisContract.address);
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+runMain();*/
