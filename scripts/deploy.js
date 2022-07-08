54b77c332f807091d5c5bb746e514b0c116a0e57
esterbetjstage <julien.esterbet@sib.fr>
2022-07-08 14:48:44 +0200

    update files

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,8 @@ async function main() {
     const [deployer] = await ethers.getSigners();
     console.log("Deploying contracts with the account:", deployer.address);
 
-    const megalisFactory = await ethers.getContractFactory("megalisV3");
+    // Change with the right contract name (V1/V2/V3)
+    const megalisFactory = await ethers.getContractFactory("megalisV1");
     const megalisContract = await megalisFactory.deploy();
     await megalisContract.deployed();
     console.log("Contract address:", megalisContract.address);
